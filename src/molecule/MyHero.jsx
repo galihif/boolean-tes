861372ab29b810722893165e4845c5051689ab54
galihif <galihindra2401@mail.ugm.ac.id>
2021-04-22 14:40:33 +0700

    add tes

--- src/molecule/MyHero.jsx
+++ src/molecule/MyHero.jsx
@@ -10,6 +10,7 @@ const MyHero = (props) => {
             <br/>
             <br/>
             <br/>
+            tes
             <Container className="tagline-container justify-content-lg-start justify-content-sm-center ml-lg-5 mt-lg-5">
                 <Row className="my-3">
                     <Col>
