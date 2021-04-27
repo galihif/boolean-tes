7580e9eb07753930a19bb6a781d52bf03402229d
galihif <galihindra2401@mail.ugm.ac.id>
2021-04-27 13:07:37 +0700

    Add about us

---
+++ src/components/MyAboutUs.jsx
@@ -0,0 +1,26 @@
+import React, { } from 'react'
+import { Button, Jumbotron, Form, Col, Container, Row } from 'react-bootstrap'
+import './MyAboutUs.scss'
+
+const MyAboutUs = (props) => {
+    return (
+        <Container className="justify-content-lg-center about-container " fluid>
+            <Row className="mt-5">
+                <Col>
+                    <h2>About Us</h2>
+                </Col>
+            </Row>
+            <br/>
+            <Row className="my-5">
+                <Col>
+                    <p>
+                        Boolean is stands for 'Booking Onlen Lapangan'. You can <br/> 
+                        book a sports field anywhere and anytime as you want
+                    </p>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default MyAboutUs
