a156416e8482ee5a14b9e4572746672fbd4ff763
galihif <galihindra2401@mail.ugm.ac.id>
2021-05-17 23:50:09 +0700

    move myfilter to venues

--- src/components/MyFilter.jsx
+++ src/components/MyFilter.jsx
@@ -1,9 +1,19 @@
-import React, { } from 'react'
+import React, {useState } from 'react'
 import { Form, Card, Container, Row, Col } from 'react-bootstrap';
 
 import './MyFilter.scss'
 
 const MyFilter = () => {
+    const [catFilter, setCatFilter] = useState([])
+
+    const handleChangeCategory = (e) => {
+        if(e.target.checked){
+            catFilter.push(e.target.id)
+        } else {
+            catFilter.splice(catFilter.indexOf(e.target.id),1)
+        }
+        console.log(catFilter)
+    }
     return (
         <Container className="filter-container d-flex justify-content-center p-0">
             <Container className="filter-box p-4">
@@ -14,24 +24,24 @@ const MyFilter = () => {
                     <h5>Category</h5>
                 </Row>
                 <Row className="pl-3 pr-3">
-                    <Form.Group controlId="formBasicCheckbox">
-                        <Form.Check type="checkbox" label="Futsal" />
-                        <Form.Check type="checkbox" label="Futsal" />
-                        <Form.Check type="checkbox" label="Futsal" />
-                        <Form.Check type="checkbox" label="Futsal" />
-                        <Form.Check type="checkbox" label="Futsal" />
+                    <Form.Group controlId="category">
+                        <Form.Check type="checkbox" label="Futsal" id="Futsal" onChange={handleChangeCategory}/>
+                        <Form.Check type="checkbox" label="Basket" id="Basket" onChange={handleChangeCategory}/>
+                        <Form.Check type="checkbox" label="Volley" id="Volley" onChange={handleChangeCategory}/>
+                        <Form.Check type="checkbox" label="Tennis" id="Tennis" onChange={handleChangeCategory}/>
+                        <Form.Check type="checkbox" label="Badminton" id="Badminton" onChange={handleChangeCategory} />
                     </Form.Group>
                 </Row>
                 <Row className="pl-3 pr-3">
                     <h5>Floor</h5>
                 </Row>
                 <Row className="pl-3 pr-3">
-                    <Form.Group controlId="formBasicCheckbox">
+                    <Form.Group controlId="floor">
                         <Form.Check type="checkbox" label="Vinyl" />
-                        <Form.Check type="checkbox" label="Futsal" />
-                        <Form.Check type="checkbox" label="Futsal" />
-                        <Form.Check type="checkbox" label="Futsal" />
-                        <Form.Check type="checkbox" label="Futsal" />
+                        <Form.Check type="checkbox" label="Synthetic Grass" />
+                        <Form.Check type="checkbox" label="Cement" />
+                        <Form.Check type="checkbox" label="Parquette" />
+                        <Form.Check type="checkbox" label="Taraflex" />
                     </Form.Group>
                 </Row>
             </Container>
--- src/pages/Venues.jsx
+++ src/pages/Venues.jsx
@@ -1,5 +1,5 @@
-import React, { Component } from 'react'
-import {Container, Row, Col, Spinner } from 'react-bootstrap';
+import React, { useState, useEffect } from 'react'
+import { Container, Row, Col, Spinner, Form } from 'react-bootstrap';
 import firebase from '../config/firebase'
 
 import './Venues.scss'
@@ -9,85 +9,118 @@ import MyFindField from '../components/MyFindField';
 import MyFilter from '../components/MyFilter';
 import MyVenueCard from '../components/MyVenueCard';
 
-class Venues extends Component {
+const Venues = () => {
+    const [venues, setVenues] = useState([])
+    const [isLoading, setLoading] = useState(false)
+    const [catFilter, setCatFilter] = useState([])
 
-    state = {
-        venues: [],
-        isLoading: false
+    useEffect(() => {
+        getVenues()
+    }, [])
+
+    const handleChangeCategory = (e) => {
+        if (e.target.checked) {
+            catFilter.push(e.target.id)
+        } else {
+            catFilter.splice(catFilter.indexOf(e.target.id), 1)
+        }
+        console.log(catFilter)
     }
 
-    getVenues(){
+    const getVenues = () => {
         const ref = firebase.firestore().collection("venues")
-        this.setState({isLoading: true})
+        setLoading(true)
         ref.onSnapshot((snapshot) => {
             const items = []
             snapshot.forEach((doc) => {
                 items.push(doc.data())
             })
-            this.setState({
-                venues: items,
-                isLoading: false
-            })
+            setVenues(items)
+            setLoading(false)
         })
     }
 
-    componentDidMount(){
-        this.getVenues()
-    }
 
-    render() {
-        const {venues, isLoading} = this.state
-
-        return (
-            <div>
-                <MyFindField/>
-                   
-                <Container className="mt-5">
-                    <Row className="justify-content-center d-flex">
-                        <Col lg={2} xs={10} className="mx-xs-auto">
-                            <MyFilter/>
-                        </Col>
-                        <Col lg={10}>
-                            <Container className="list-venue-container">
-                                {
-                                    isLoading ? (
-                                        <Row className="d-flex justify-content-center align-items-center">
-                                            <Spinner animation="border" role="status">
-                                                <span className="sr-only">Loading...</span>
-                                            </Spinner>
-                                            <br /><br /><br /><br /><br /><br /><br /><br />
-                                            <br /><br /><br /><br /><br /><br /><br /><br />
-                                        </Row>
-                                    ) : null
-                                }
-                                <Row className="d-flex justify-content-left">
-                                    {
-                                        venues.map((venue) => {
-                                            return (
-                                                <Col lg={4} className="px-3 mx-0 mb-3">
-                                                    <MyVenueCard
-                                                        key={venue.venueId}
-                                                        venueName={venue.venueName}
-                                                        venueId={venue.venueId}
-                                                        numberOfFields={venue.numberOfFields}
-                                                        venueAddress={venue.venueAddress}
-                                                        venueSportType={venue.venueSportType}
-                                                        venueImage={venue.venueImage}
-                                                        venueRating={venue.venueRating}
-                                                    />
-                                                </Col>
-                                            )
-                                        })
-                                        
-                                    }
+    return (
+        <div>
+            <MyFindField/>
+                
+            <Container className="mt-5">
+                <Row className="justify-content-center d-flex">
+                    <Col lg={2} xs={10} className="mx-xs-auto">
+                        <Container className="filter-container d-flex justify-content-center p-0">
+                            <Container className="filter-box p-4">
+                                <Row className="d-flex justify-content-center">
+                                    <h3>Filter</h3>
+                                </Row>
+                                <Row className="pl-3 pr-3">
+                                    <h5>Category</h5>
+                                </Row>
+                                <Row className="pl-3 pr-3">
+                                    <Form.Group controlId="category">
+                                        <Form.Check type="checkbox" label="Futsal" id="Futsal" onChange={handleChangeCategory} />
+                                        <Form.Check type="checkbox" label="Basket" id="Basket" onChange={handleChangeCategory} />
+                                        <Form.Check type="checkbox" label="Volley" id="Volley" onChange={handleChangeCategory} />
+                                        <Form.Check type="checkbox" label="Tennis" id="Tennis" onChange={handleChangeCategory} />
+                                        <Form.Check type="checkbox" label="Badminton" id="Badminton" onChange={handleChangeCategory} />
+                                    </Form.Group>
+                                </Row>
+                                <Row className="pl-3 pr-3">
+                                    <h5>Floor</h5>
+                                </Row>
+                                <Row className="pl-3 pr-3">
+                                    <Form.Group controlId="floor">
+                                        <Form.Check type="checkbox" label="Vinyl" />
+                                        <Form.Check type="checkbox" label="Synthetic Grass" />
+                                        <Form.Check type="checkbox" label="Cement" />
+                                        <Form.Check type="checkbox" label="Parquette" />
+                                        <Form.Check type="checkbox" label="Taraflex" />
+                                    </Form.Group>
                                 </Row>
                             </Container>
-                        </Col>
-                    </Row>
-                </Container>
-            </div>
-        )
-    }
+                        </Container>
+                    </Col>
+                    <Col lg={10}>
+                        <Container className="list-venue-container">
+                            {
+                                isLoading ? (
+                                    <Row className="d-flex justify-content-center align-items-center">
+                                        <Spinner animation="border" role="status">
+                                            <span className="sr-only">Loading...</span>
+                                        </Spinner>
+                                        <br /><br /><br /><br /><br /><br /><br /><br />
+                                        <br /><br /><br /><br /><br /><br /><br /><br />
+                                    </Row>
+                                ) : (
+                                        <Row className="d-flex justify-content-left">
+                                            {
+                                                venues.map((venue) => {
+                                                    return (
+                                                        <Col lg={4} className="px-3 mx-0 mb-3">
+                                                            <MyVenueCard
+                                                                key={venue.venueId}
+                                                                venueName={venue.venueName}
+                                                                venueId={venue.venueId}
+                                                                numberOfFields={venue.numberOfFields}
+                                                                venueAddress={venue.venueAddress}
+                                                                venueSportType={venue.venueSportType}
+                                                                venueImage={venue.venueImage}
+                                                                venueRating={venue.venueRating}
+                                                            />
+                                                        </Col>
+                                                    )
+                                                })
+
+                                            }
+                                        </Row>
+                                )
+                            }
+                        </Container>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    )
 }
 
 export default Venues
