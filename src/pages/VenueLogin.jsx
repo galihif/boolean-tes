2bb10f1ee4ae09a696d34e2d5411d0307d664357
galihif <galihindra2401@mail.ugm.ac.id>
2021-06-06 21:46:26 +0700

    add venuelogin

---
+++ src/pages/VenueLogin.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from 'react'
+import { Col, Container, Form, Button } from 'react-bootstrap'
+import { Link, useHistory } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+
+import './Login.scss'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import firebase from '../config/firebase'
+
+const VenueLogin = () => {
+    const dispatch = useDispatch()
+    const state = useSelector((state) => state)
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+    let history = useHistory()
+
+    const handleChange = (e) => {
+        switch(e.target.id){
+            case "email":
+                setEmail(e.target.value)
+                break
+            case "password":
+                setPassword(e.target.value)
+                break
+            default:
+                return null
+        }
+    }
+
+    const handleSubmit = () => {
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .then((userCredential) => {
+                var user = userCredential.user
+                history.push(`/venueowner/${user.uid}`)
+                dispatch({ type: "LOGIN", data: user.uid })
+            })
+            .catch((error) => {
+                var errorCode = error.code
+                var errorMessage = error.message
+                console.log(errorCode, errorMessage)
+                setError(errorMessage)
+            })
+    }
+    return (
+            <Container className="justify-content-center d-flex py-5">
+                <Col lg={6}>
+                    <div className="login-card">
+                        <h3 className="text-center">Login as Venue Owner</h3>
+                        <Form>
+                            <Form.Group controlId="email">
+                                <Form.Label>Email address</Form.Label>
+                                <Form.Control type="email" placeholder="Enter email" onChange={handleChange}/>
+                            </Form.Group>
+
+                            <Form.Group controlId="password">
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control type="password" placeholder="Password" onChange={handleChange}/>
+                            </Form.Group>
+                            <Button variant="primary" type="" className="btn-block" onClick={handleSubmit}>
+                                Login
+                            </Button>
+                            {
+                                error !== "" ? <p className="text-danger mt-2 text-center">{error}</p> : null
+                            }
+                            <br/>
+                            <p>Doesn't have an account?  <Link to="/venueregister">Join us</Link></p>
+                        </Form>
+                    </div>
+                </Col>
+            </Container>
+    )
+    
+}
+
+export default VenueLogin
